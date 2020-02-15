d1dbec2721c9a2e17c088a2035e4929c5175736d
Lelianto Eko Pradana <lian@alterra.id>
2020-02-15 21:14:11 +0700

    try google sign in

--- public/logInGoogle.js
+++ public/logInGoogle.js
@@ -4,15 +4,12 @@ async function onLogIn(googleUser){
     const pureUsername = profile.getEmail()
     const splitPureUsername = pureUsername.split('@')
     const username = splitPureUsername[0]
-    console.log(username)
-    console.log(profile)
-    console.log('token', googleUser.getAuthResponse())
     if(localStorage.getItem('status')){
         localStorage.setItem('gmail_username', username)
         localStorage.setItem('gmail_email', profile.getEmail())
         localStorage.setItem('gmail_token', id_token)
         localStorage.setItem('google', true)
         localStorage.removeItem('status')
-        window.location.replace('http://localhost:3000/masuk')
+        window.location.replace('https://kodekula.com/masuk')
     }
 }
